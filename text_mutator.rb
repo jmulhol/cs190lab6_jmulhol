c1552acb712edf7a9d74b6f49fbf8f0c20d3837f
Jace Mulholland <jace_mulholland@yahoo.com>
2013-11-22 17:04:21 -0500

    fixes bug with text input, changes source from argv to user input

--- text_mutator.rb
+++ text_mutator.rb
@@ -9,7 +9,9 @@ def main
   modification = ARGV[0]
 
   # store input text into text variable
-  text = ARGV[1]
+  puts "Input some text, please"
+
+  text = $stdin.gets.chomp
 
   # modify text accordingly
   case modification
